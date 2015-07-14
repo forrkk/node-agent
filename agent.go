4802d79193261479ec8657e1b88d9a52d571439a
Ivan Gaas <ig@wodby.com>
2015-07-14 18:17:31 +0600

    - some code

--- agent.go
+++ agent.go
@@ -3,25 +3,43 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"runtime"
 )
 
 func main() {
-	if m, err := GetOsInfo(); err == nil {
-		fmt.Println(m["type"])
-		fmt.Println(m["arch"])
-		fmt.Println(m["pretty_name"])
-		fmt.Println(m["id"])
-		fmt.Println(m["version_id"])
-		fmt.Println(m["id_like"])
-		fmt.Println(m["kernel_ver"])
-		fmt.Println(m["init1"])
+	//os := make(map[string]string)
+	switch runtime.GOOS {
+	case "linux": break
+	default: log.Fatalln("not implemented")
 	}
-	if m, err := GetDockerStatus(); err == nil {
-		fmt.Println(m["docker_path"])
-		fmt.Println(m["docker_version"])
-		fmt.Println(m["docker_running"])
+	if _, err := GetOsInfo(); err != nil {
+		log.Fatalln(err)
 	}
-	p := []int{22,23}
-	fmt.Println(IsRoot())
-	fmt.Println(GetPortAvailability(p))
+	if !IsRoot() {
+		log.Fatalln("must be root")
+	}
+	err := GetPortAvailability([]int{22, 80, 443})
+	if err == nil {
+		fmt.Println(err)
+	}
+
+//	if m, err := GetOsInfo(); err == nil {
+//		fmt.Println(m["type"])
+//		fmt.Println(m["arch"])
+//		fmt.Println(m["pretty_name"])
+//		fmt.Println(m["id"])
+//		fmt.Println(m["version_id"])
+//		fmt.Println(m["id_like"])
+//		fmt.Println(m["kernel_ver"])
+//		fmt.Println(m["init1"])
+//	}
+//	if m, err := GetDockerStatus(); err == nil {
+//		fmt.Println(m["docker_path"])
+//		fmt.Println(m["docker_version"])
+//		fmt.Println(m["docker_running"])
+//	}
+//	p := []int{22,23}
+//	fmt.Println(IsRoot())
+//	fmt.Println(GetPortAvailability(p))
 }
---
+++ config.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/BurntSushi/toml"
+)
+
+type Config struct {
+
+}
